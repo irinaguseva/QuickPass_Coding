11634109e427a9d2f71ceac3822d2d88f0b1c8ce
irgus <85966150+irinaguseva@users.noreply.github.com>
2023-01-04 16:19:57 +0300

    Add files via upload

---
+++ QuickPassTasks _11.py
@@ -0,0 +1,30 @@
+# QuickPassTasks 11
+import os
+
+dr = input()
+files = os.listdir(dr)
+
+file_for_answer = input()
+ans = open(file_for_answer, 'w')
+
+for name in files:
+    
+    path = dr + '/' + name
+    with open(path, 'r') as f:
+        file = f.readlines()
+    lines = len(file)
+    syms = 0
+    words = 0
+    
+    for line in file:   
+        line = line.rstrip('\n')
+        syms += len(line)
+        
+        line = line.split()
+        words += len(line)
+    
+    ans.write('|************' + name + '************|\n')
+    ans.write('Количество символов: ' + str(syms) + '\n')
+    ans.write('Количество слов: ' + str(words) + '\n')
+    ans.write('Количество строк: ' + str(lines) + '\n')
+ans.close()
